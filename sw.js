786e206aad5ce7f28b424124c3470f147903827a
eldisamuranuzla-cmd <eldisamuranuzla@gmail.com>
2026-09-15 13:44:36 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='amira-v25-csv-spray-type-fix';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./astra-agro-50-header.png','./astra-agro-logo-compact.png','./astra-agro-logo.png']))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
